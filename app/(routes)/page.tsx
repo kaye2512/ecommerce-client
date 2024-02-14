6f2799a0870b6cd98c0e5e1aeda10ec471bf489b
kaye <kayeromy@gmail.com>
2024-02-14 15:03:54 +0100

    product addtocart next cart page to do

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/ui/billboard";
+import Container from "@/components/ui/container";
+import getProduct from "@/actions/get-products";
+import ProductList from "@/components/product-list";
 
-const HomePage = () => {
+export const revalidate = 0;
+
+const HomePage = async () => {
+    const products = await getProduct({isFeatured: true});
+    const billboard = await getBillboard("d63974b1-8049-4711-9694-fd34f9584cc0");
+
     return (
-        <div>
-            Hello Store
-        </div>
-    );
+        <Container>
+            <div className="space-y-10 pb-10">
+                <Billboard
+                    data={billboard}
+                />
+                <div className={"flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8"}>
+                    <ProductList title={"Featured Product"} items={products}/>
+                </div>
+            </div>
+
+        </Container>
+    )
 };
 
 export default HomePage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/footer";
+import Navbar from "@/components/navbar";
+import ModalProvider from "@/providers/modal-provider";
+import ToastProvider from "@/providers/toast-provider";
 
 const font = Urbanist({ subsets: ["latin"] });
 
@@ -16,7 +20,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+      <ModalProvider/>
+      <ToastProvider/>
+        <Navbar/>
+            {children}
+        <Footer />
+      </body>
     </html>
   );
 }
