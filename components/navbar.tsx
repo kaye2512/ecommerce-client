0cd7512972bd7b7448988336a8a6b48cb4fbc0f4
kaye <kayeromy@gmail.com>
2024-02-15 17:01:21 +0100

    final

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,28 +1,24 @@
-import React from 'react';
-import Container from "@/components/ui/container";
 import Link from "next/link";
-import MainNav from "@/components/main-nav";
-import getCategories from "@/actions/get-categories";
-import NavbarActions from "@/components/navbar-actions";
 
-export const revalidate = 0;
+import MainNav from "@/components/main-nav";
+import Container from "@/components/ui/container";
+import NavbarActions from "@/components/navbar-actions";
+import getCategories from "@/actions/get-categories";
+
 const Navbar = async () => {
     const categories = await getCategories();
 
-
     return (
-        <div className={"border-b"}>
+        <div className="border-b">
             <Container>
-                <div className={"relative px-4 sm:px-6 lg:px-8 flex h-16 items-center"}>
-                    <Link className={"ml-4 flex lg:ml-0 gap-x-2"} href={"/"}>
-                        <p className={"font-bold text-xl"}>STORE</p>
+                <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
+                    <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
+                        <p className="font-bold text-xl">STORE</p>
                     </Link>
-                    <MainNav data={categories}/>
-                    <NavbarActions/>
+                    <MainNav data={categories} />
+                    <NavbarActions />
                 </div>
-
             </Container>
-
         </div>
     );
 };
